4ef5b79277ae5421ca67dd93a4f6825c118e4dad
unknown <natnaelm552@gmail.com>
2025-01-04 16:40:37 +0300

    sqlite database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 
 @Module({
@@ -11,7 +13,7 @@ import {TypeOrmModule} from '@nestjs/typeorm';
     {
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User , Report],
       synchronize: true
     }
   ) ,UsersModule, ReportsModule],
