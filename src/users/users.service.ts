3200a076f33d62f0e6d11655284dc4d514e1143c
unknown <natnaelm552@gmail.com>
2025-01-05 10:41:15 +0300

    updating a user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,12 @@ export class UsersService {
     const user = this.repo.create({email , password});
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email:string){
+    return this.repo.find({where : {email}});
+  }
 }
