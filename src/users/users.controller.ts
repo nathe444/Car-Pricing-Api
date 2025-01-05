71764a3376dcd92c5a6254bd5c428828f363bd5e
unknown <natnaelm552@gmail.com>
2025-01-05 11:16:29 +0300

    crud on a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post , Get, Param, Query ,Delete,Patch} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { parse } from 'path';
 
 @Controller('auth')
 export class UsersController {
@@ -10,6 +12,26 @@ export class UsersController {
   @Post('/signUp')
   createUser(@Body() body:CreateUserDto){
     this.userService.create(body.email , body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id:string){
+    return this.userService.findOne(parseInt(id))
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email:string){
+    return this.userService.find(email)
+  }
+
+  @Patch('/:id')
+  updateUser(@Body() body:UpdateUserDto , @Param('id') id:string){
+    return this.userService.update(parseInt(id) , body)
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id:string){
+    return this.userService.remove(parseInt(id))
   }
 
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,4 +20,21 @@ export class UsersService {
   find(email:string){
     return this.repo.find({where : {email}});
   }
+
+  async update(id:number , attrs: Partial<User>){
+    const user = await this.findOne(id)
+    if(!user){
+      throw new Error("user not found")
+    }
+    Object.assign(user , attrs)
+    return this.repo.save(user);
+  }
+
+  async remove(id:number){
+    const user = await this.repo.findOneBy({id})
+    if(!user){
+      throw new Error("user not found")
+    }
+    return this.repo.remove(user)
+  }
 }
